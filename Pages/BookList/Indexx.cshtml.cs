d5a728fd9a68daf1ce58553adeb884fa295d1517
nattysha <nattynattyha@gmail.com>
2021-05-17 00:06:40 -0700

    Book list Razor

---
+++ Pages/BookList/Indexx.cshtml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookListRazor.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookListRazor.Pages.BookList
+{
+    public class IndexxModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+
+        public IndexxModel(ApplicationDbContext db)
+        {
+            _db = db;
+
+        }
+        public IEnumerable<Book> Books { get; set; }
+
+
+        public async Task OnGet()
+        {
+            Books = await _db.Book.ToListAsync();     
+        }
+        public async Task<IActionResult> OnPostDelet(int id)
+        {
+            var book = await _db.Book.FindAsync(id);
+            if(book == null)
+            {
+                return NotFound();
+            }
+            _db.Book.Remove(book);
+            await _db.SaveChangesAsync();
+
+            return RedirectToPage("Indexx");
+
+        }
+    }
+}
